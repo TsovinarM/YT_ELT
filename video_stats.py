e504ffd4851e5cb9829cec130509589bdef6bca5
Tsovinar <tsovinarmelkumyan1@gmail.com>
2025-11-25 20:30:38 +0400

    geting video_ids

--- video_stats.py
+++ video_stats.py
@@ -7,8 +7,9 @@ load_dotenv('./.env')
 
 API_KEY = os.getenv('API_KEY')
 CHANEL_HANDLE = 'MrBeast&key'
+maxResults = 50
 
-def get_video_count():
+def get_playlist_id():
     try:
         url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANEL_HANDLE}={API_KEY}'
 
@@ -24,6 +25,40 @@ def get_video_count():
         return channel_playlistId
     except requests.exceptions.RequestException as e:
         raise e
-    
+
+
+
+def get_video_ids(playlistId):
+    video_ids = []
+    pageToken = None
+    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}'
+    # base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId=UUX6OQ3DkcsbYNE6H8uQQuVA&key={API_KEY}'
+
+    try:
+        while True:
+            url = base_url
+            
+            if pageToken:
+                url += f'&pageToken={pageToken}'
+                
+            responce = requests.get(url)
+            responce.raise_for_status()
+            data = responce.json()
+            
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+                print(1)
+                
+            pageToken = data.get('nextPageToken')
+            if not pageToken:
+                break
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e 
+
+
 if __name__ == '__main__':
-    get_video_count()
+    playlistId = get_playlist_id() 
+    get_video_ids(playlistId)
